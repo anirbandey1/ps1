d8821e3049d1228a384bd1ce9a42ab827989906e
Anirban Dey <116617392+anirbandey1@users.noreply.github.com>
2023-08-19 18:16:55 +0530

    path.ps1 for changing $env:path

--- p1.ps1
+++ p1.ps1
@@ -1,2 +1,8 @@
+
 Write-Host "Hello World"
 Write-Host "Again"
+
+$uri = "https://github.com/anirbandey1/openai-client/releases/download/v1.0.0/openai-client.exe"
+$dest = "openai-client.exe"
+
+Invoke-WebRequest -Uri  $uri -OutFile $dest
